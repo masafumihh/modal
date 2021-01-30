549d8dd17f378fbd609d558ee13313343c2dfaf5
堀内雅文 <masafumi@horiuchimasafuminoMacBook-Pro.local>
2021-01-30 11:35:03 +0900

    first commmit

---
+++ script.js
@@ -0,0 +1,51 @@
+(() => {
+    const openModalBtn = document.querySelectorAll('[data-open-modal]');
+    const closeModalBtn = document.querySelectorAll('[data-close-modal]');
+    const closeAccountModalBtn = document.querySelectorAll('[data-close-account-modal]');
+    const overlay = document.querySelector('[data-overlay]');
+    
+    openModalBtn.forEach(btn => {
+        btn.addEventListener('click', () => {
+            const modal = document.querySelector(btn.dataset.openModal);
+            openModal(modal);
+        });
+    });
+
+    closeModalBtn.forEach(btn => {
+        btn.addEventListener('click', () => {
+            const modal = btn.closest('.active');
+            closeModal(modal);
+        });
+    });
+
+    closeAccountModalBtn.forEach(btn => {
+        btn.addEventListener('click', () => {
+            const modal = btn.closest('.active');
+            closeAccountModal(modal);
+        });
+    });
+    
+    overlay.addEventListener('click', () => {
+        const modals = document.querySelectorAll('.modal.active');
+        modals.forEach(modal => {
+            closeModal(modal);
+        });
+    });
+
+    function openModal(modal) {
+        if (modal == null) return;
+        modal.classList.add('active');
+        overlay.classList.add('active');
+    }
+
+    function closeModal(modal) {
+        if (modal == null) return;
+        modal.classList.remove('active');
+        overlay.classList.remove('active');
+    }
+
+    function closeAccountModal(modal) {
+        modal.classList.remove('active');
+    }
+
+})();
